3d83b469aa79a5425a2419f4f17485adb14523ba
Anjali Patel <anjali31017@gmail.com>
2024-11-13 20:27:46 +0530

    Model creation

--- graduation/models.py
+++ graduation/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
 
-class newAdmission(AbstractUser):
+class NewAdmission(AbstractUser):
     COURSE_CATEGORY = [
         ('CS','Computer Science'),
         ('IT','Information Technology'),
         ('CA', 'Computer Applications')
     ]
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=30, blank=True)
+    username = models.CharField(unique=True, max_length=30, blank=True)
     email = models.CharField(max_length=50, blank=True)
     password = models.CharField(max_length=50, blank=True)
-    firstName = models.CharField(max_length=30, blank=True)
-    lastName = models.CharField(max_length=30, blank=True)
-    percent_12 = models.DecimalField(max_digits=4, blank=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    percent_12 = models.DecimalField(max_digits=4, blank=True, decimal_places=2)
     course = models.CharField(max_length=30,choices=COURSE_CATEGORY, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
--- student/settings.py
+++ student/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv;
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,3 +136,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+AUTH_USER_MODEL = 'graduation.NewAdmission'
